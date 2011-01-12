dd0750e42cb30c3c1de02f9fdf0a19667fe7c170
lionel.r.joseph@gmail.com <lionel.r.joseph@gmail.com@aa75e4b2-45b2-6c6e-9ac0-30496596d394>
2011-01-12 17:00:27 +0000

    - Lecture complète d'un niveau au format XML sans erreur. - A venir : rajout d'informations dans le fichier

--- trunk/Mario Project/Mario Project/world.c
+++ trunk/Mario Project/Mario Project/world.c
@@ -121,9 +121,9 @@ void load_world(world *w)
 {
 	/* Chargement du niveau */
     //charger_niveau_test_vide(w->niveau);
-	charger_niveau_test(w->niveau);
+	//charger_niveau_test(w->niveau);
 	//affiche_occ_blocs(w->niveau);
-	//charger_niveau("test_xml.xml", w->niveau);
+	charger_niveau("test_xml.xml", w->niveau);
 	charger_textures_niveau(w->niveau);
 	
 	/* Initialisation du nom et du temps dans le HUD pour chaque niveau chargé */
